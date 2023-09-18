51c19802058d7cb54bff2a0ae878255194be813d
Leo T <leonard.t@bealy.io>
2023-09-18 17:22:19 +0200

    found better way to deal with epoch unix millis and added / time.now

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 // ? clean this
 func timestampEndpoint(c *gin.Context) {
-	fmt.Println(c.Request.UserAgent(), c.ClientIP())
+	fmt.Println(c.Request.UserAgent(), c.ClientIP()) //! erase later, add this to mdw
 	timestamp := c.Param("timestamp")
 	normalTimestamp, err := time.Parse("2006-01-02", timestamp)
 	if err == nil {
@@ -26,10 +26,9 @@ func timestampEndpoint(c *gin.Context) {
 	}
 	timestampUnix, err := strconv.ParseInt(timestamp, 10, 64)
 	if err == nil {
-		timestampUnix /= 1000
-		dtUnix := time.Unix(timestampUnix, 0)
+		dtUnix := time.UnixMilli(timestampUnix)
 		c.JSON(http.StatusOK, gin.H{
-			"unix": timestampUnix * 1000,
+			"unix": timestampUnix,
 			"utc":  dtUnix.UTC().Format(http.TimeFormat),
 		})
 		return
@@ -39,7 +38,16 @@ func timestampEndpoint(c *gin.Context) {
 	})
 }
 
+func noTimestampEndpoint(c *gin.Context) {
+	timestamp := time.Now().UTC()
+	c.JSON(http.StatusOK, gin.H{
+		"unix": timestamp.UnixMilli(),
+		"utc":  timestamp.Format(http.TimeFormat),
+	})
+}
+
 func SetupRoutes(router *gin.RouterGroup) {
+	router.GET("/", noTimestampEndpoint)
 	router.GET("/:timestamp", timestampEndpoint)
 }
 
