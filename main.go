b220266b6fad7a323d89e1e97c8cc904dec6815f
Leo T <leonard.t@bealy.io>
2023-09-18 16:36:14 +0200

    added endpoint, and some comments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -8,44 +9,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//? Can add just this and it will work to router.GET() -> use like this and call it endpoint
-// func test(c *gin.Context) {
-// 	currentTime := time.Now().UTC().Format(time.RFC822)
-// 	timestamp := c.Param("timestamp")
-// 	fmt.Println("The time is", currentTime, timestamp, c.ClientIP())
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"unix": currentTime,
-// 		"utc":  currentTime,
-// 	})
-// }
+//? add middleware that will log analytics
 
-// ? move this to its own endpoint on top of this
-func SetupRoutes(router *gin.RouterGroup) {
-	router.GET("/:timestamp", func(c *gin.Context) {
-		timestamp := c.Param("timestamp")
-		normalTimestamp, err := time.Parse("2006-01-02", timestamp)
-		if err == nil {
-			unixTime := normalTimestamp.UTC().Unix() * 1000
-			c.JSON(http.StatusOK, gin.H{
-				"unix": unixTime,
-				"utc":  normalTimestamp.UTC().Format(http.TimeFormat),
-			})
-			return
-		}
-		timestampUnix, err := strconv.ParseInt(timestamp, 10, 64)
-		if err == nil {
-			timestampUnix /= 1000
-			dtUnix := time.Unix(timestampUnix, 0)
-			c.JSON(http.StatusOK, gin.H{
-				"unix": timestampUnix * 1000,
-				"utc":  dtUnix.UTC().Format(http.TimeFormat),
-			})
-			return
-		}
+// ? clean this
+func timestampEndpoint(c *gin.Context) {
+	fmt.Println(c.Request.UserAgent(), c.ClientIP())
+	timestamp := c.Param("timestamp")
+	normalTimestamp, err := time.Parse("2006-01-02", timestamp)
+	if err == nil {
+		unixTime := normalTimestamp.UTC().Unix() * 1000
 		c.JSON(http.StatusOK, gin.H{
-			"error": "invalid Date",
+			"unix": unixTime,
+			"utc":  normalTimestamp.UTC().Format(http.TimeFormat),
 		})
+		return
+	}
+	timestampUnix, err := strconv.ParseInt(timestamp, 10, 64)
+	if err == nil {
+		timestampUnix /= 1000
+		dtUnix := time.Unix(timestampUnix, 0)
+		c.JSON(http.StatusOK, gin.H{
+			"unix": timestampUnix * 1000,
+			"utc":  dtUnix.UTC().Format(http.TimeFormat),
+		})
+		return
+	}
+	c.JSON(http.StatusBadRequest, gin.H{
+		"error": "invalid Date",
 	})
+}
+
+func SetupRoutes(router *gin.RouterGroup) {
+	router.GET("/:timestamp", timestampEndpoint)
 }
 
 func main() {
