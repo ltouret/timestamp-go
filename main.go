51e0aea270e74160fe0135ca00da8d1e50f849a5
Leo T <leonard.t@bealy.io>
2023-09-19 16:03:01 +0200

    wip

--- main.go
+++ main.go
@@ -6,8 +6,18 @@ import (
 	"strconv"
 	"time"
 
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func truncateText(s string, max int) string {
+	if max > len(s) {
+		return s
+	}
+	return s[:max]
+}
 
 // ? add middleware that will log analytics
 // use dependecy injection to add db here?
@@ -65,9 +75,51 @@ func SetupRoutes(router *gin.RouterGroup) {
 	router.GET("/:timestamp", timestampEndpoint)
 }
 
+// ? add in .env
+// DB_USER = root
+// DB_PASS =
+func SetupDb() {
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/")
+	if err != nil {
+		fmt.Println("Connection Db", err)
+	}
+	defer db.Close()
+
+	// Check if the database exists
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS analytics")
+	if err != nil {
+		fmt.Println("Create Db", err)
+	}
+
+	// Switch to the newly created database
+	_, err = db.Exec("USE analytics")
+	if err != nil {
+		fmt.Println("Use Db", err)
+	}
+
+	// Create the table if it doesn't exist
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS test (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            userAgent VARCHAR(255) DEFAULT NULL,
+            clientIP VARCHAR(255) DEFAULT NULL,
+            timestamp VARCHAR(255) DEFAULT NULL,
+            responseTime VARCHAR(255) DEFAULT NULL,
+            queryParameters VARCHAR(255) DEFAULT NULL,
+			userUuid CHAR(36) DEFAULT NULL
+        )
+    `)
+	if err != nil {
+		fmt.Println("Create Table", err)
+	}
+
+	fmt.Println("Database and table initialization completed.")
+}
+
 func main() {
+	SetupDb() //? add here or in another main...
 	router := gin.Default()
-	router.Use(gin.Recovery()) // useful?
+	router.Use(gin.Recovery()) // useful for 500 error do i keep this?
 	apiGroup := router.Group("/api")
 	v1 := apiGroup.Group("/v1", v1MiddlewareTest())
 	SetupRoutes(v1)
